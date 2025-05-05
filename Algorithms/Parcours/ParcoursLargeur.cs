e1a22ac622d70964ddbb59fc1b39c44da7da8794
max3000aez <maxime.mottier@edu.devinci.fr>
2025-05-05 14:35:56 +0200

    parcours

--- Algorithms/Parcours/ParcoursLargeur.cs
+++ Algorithms/Parcours/ParcoursLargeur.cs
@@ -1,4 +1,58 @@
 using System;
 using System.Collections.Generic;
+using Gra = TransConnect.Models.Graphe;
 
+namespace TransConnect.Algorythme.Parcours
+{
+    public class ParcoursLargeur
+    {
+        private Gra.Graphe _graphe;
 
+        public ParcoursLargeur(Gra.Graphe graphe)
+        {
+            _graphe = graphe;
+        }
+
+        public List<Gra.Noeud> Parcourir(Gra.Noeud depart)
+        {
+            var visites = new HashSet<Gra.Noeud>();
+            var file = new Queue<Gra.Noeud>();
+            var resultat = new List<Gra.Noeud>();
+
+            file.Enqueue(depart);
+            visites.Add(depart);
+
+            while (file.Count > 0)
+            {
+                var courant = file.Dequeue();
+                resultat.Add(courant);
+
+                foreach (var voisin in ObtenirVoisins(courant))
+                {
+                    if (!visites.Contains(voisin))
+                    {
+                        visites.Add(voisin);
+                        file.Enqueue(voisin);
+                    }
+                }
+            }
+
+            return resultat;
+        }
+
+        private List<Gra.Noeud> ObtenirVoisins(Gra.Noeud noeud)
+        {
+            var voisins = new List<Gra.Noeud>();
+
+            foreach (var lien in _graphe.Liens)
+            {
+                if (lien.Noeud1 == noeud)
+                    voisins.Add(lien.Noeud2);
+                else if ((lien.Oriente == null || lien.Oriente == false) && lien.Noeud2 == noeud)
+                    voisins.Add(lien.Noeud1);
+            }
+
+            return voisins;
+        }
+    }
+}
--- Algorithms/Parcours/ParcoursProfondeur.cs
+++ Algorithms/Parcours/ParcoursProfondeur.cs
@@ -1,11 +1,53 @@
 using System;
 using System.Collections.Generic;
-using TransConnect.Models.Graphe;
+using Gra = TransConnect.Models.Graphe;
 
 namespace TransConnect.Algorithms.Parcours
 {
     public class ParcoursProfondeur
     {
-        
+        private Gra.Graphe _graphe;
+
+        public ParcoursProfondeur(Gra.Graphe graphe)
+        {
+            _graphe = graphe;
+        }
+
+        public List<Gra.Noeud> Parcourir(Gra.Noeud depart)
+        {
+            var visites = new HashSet<Gra.Noeud>();
+            var resultat = new List<Gra.Noeud>();
+            Explorer(depart, visites, resultat);
+            return resultat;
+        }
+
+        private void Explorer(Gra.Noeud courant, HashSet<Gra.Noeud> visites, List<Gra.Noeud> resultat)
+        {
+            visites.Add(courant);
+            resultat.Add(courant);
+
+            foreach (var voisin in ObtenirVoisins(courant))
+            {
+                if (!visites.Contains(voisin))
+                {
+                    Explorer(voisin, visites, resultat);
+                }
+            }
+        }
+
+        private List<Gra.Noeud> ObtenirVoisins(Gra.Noeud noeud)
+        {
+            var voisins = new List<Gra.Noeud>();
+
+            foreach (var lien in _graphe.Liens)
+            {
+                if (lien.Noeud1 == noeud)
+                    voisins.Add(lien.Noeud2);
+                else if ((lien.Oriente == null || lien.Oriente == false) && lien.Noeud2 == noeud)
+                    voisins.Add(lien.Noeud1);
+            }
+
+            return voisins;
+        }
     }
 }
